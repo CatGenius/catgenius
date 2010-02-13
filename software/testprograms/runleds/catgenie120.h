8a79a7e16bd2052072794db70f0f5da056619e10
CatGenius <robert@delien.nl>
2010-02-13 20:15:07 +0000

    Buttons added

---
+++ software/testprograms/runleds/catgenie120.h
@@ -0,0 +1,27 @@
+/******************************************************************************/
+/* File    :	catgenie120.h						      */
+/* Function:	Include file of 'catgenie120.c'.			      */
+/* Author  :	Robert Delien						      */
+/*		Copyright (C) 2010, Clockwork Engineering		      */
+/******************************************************************************/
+
+#ifndef CATGENIE120_H			/* Include file already compiled? */
+#define CATGENIE120_H
+
+void		init_catgenie		(void) ;
+void		do_catgenie		(void) ;
+void		term_catgenie		(void) ;
+
+void		startbutton_isr		(void) ;
+void		setupbutton_isr		(void) ;
+void		catsensor_isr		(void) ;
+
+void		set_LED			(unsigned char led,
+					 unsigned char on);
+void		set_LED_Cat		(unsigned char on);
+void		set_LED_Error		(unsigned char on);
+void		set_LED_Cartridge	(unsigned char on);
+void		set_LED_Locked		(unsigned char on);
+void		set_Beeper		(unsigned char on);
+
+#endif /* CATGENIE120_C */
