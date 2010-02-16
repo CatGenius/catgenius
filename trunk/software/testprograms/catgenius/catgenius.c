4699f902115dc5794ea76eef98e2297330e217fe
robert <robert@fd22e926-ac0d-87e4-e151-82697668f4d6>
2010-02-16 20:07:29 +0000

    Initial revision of empty CatGenius application added

---
+++ trunk/software/testprograms/catgenius/catgenius.c
@@ -0,0 +1,166 @@
+/******************************************************************************/
+/* File    :	catgenius.c						      */
+/* Function:	CatGenius Application					      */
+/* Author  :	Robert Delien						      */
+/*		Copyright (C) 2010, Clockwork Engineering		      */
+/* History :	16 Feb 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+#include <htc.h>
+
+#include "../common/timer.h"
+#include "../common/catsensor.h"
+#include "../common/catgenie120.h"
+
+
+/******************************************************************************/
+/* Macros								      */
+/******************************************************************************/
+
+__CONFIG(XT & WDTDIS & PWRTEN & BOREN & LVPDIS & DPROT & WRTEN & PROTECT);
+//__CONFIG(XT & WDTEN & PWRTEN & BOREN & LVPDIS & DPROT & WRTEN & PROTECT);
+
+
+/******************************************************************************/
+/* Global Data								      */
+/******************************************************************************/
+
+static unsigned char	PORTB_old;
+
+
+/******************************************************************************/
+/* Local Prototypes							      */
+/******************************************************************************/
+
+static void interrupt_init (void);
+
+
+/******************************************************************************/
+/* Global Implementations						      */
+/******************************************************************************/
+
+void main (void)
+{
+	unsigned char	flags;
+
+	/* Init the hardware */
+	flags = catgenie_init();
+
+	/* Initialize software timers */
+	timer_init();
+
+	/* Initialize the cat sensor */
+	catsensor_init();
+
+	/* Initialize interrupts */
+	interrupt_init();
+
+	/* Execute the run loop */
+	for(;;){
+		catsensor_work();
+		catgenie_work();
+	}
+}
+
+void catsensor_event (unsigned char detected)
+/******************************************************************************/
+/* Function:	catsensor_event						      */
+/*		- Handle state changes of cat sensor			      */
+/* History :	13 Feb 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+}
+/* catsensor_event */
+
+void watersensor_event (unsigned char detected)
+/******************************************************************************/
+/* Function:	watersensor_event					      */
+/*		- Handle state changes of water sensor			      */
+/* History :	13 Feb 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+}
+/* watersensor_event */
+
+void heatsensor_event (unsigned char detected)
+/******************************************************************************/
+/* Function:	heatsensor_event					      */
+/*		- Handle state changes of over-heat sensor		      */
+/* History :	13 Feb 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+}
+/* heatsensor_event */
+
+void startbutton_event (unsigned char up)
+/******************************************************************************/
+/* Function:	startbutton_event					      */
+/*		- Handle state changes of Start button			      */
+/* History :	16 Feb 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+}
+/* startbutton_event */
+
+void setupbutton_event (unsigned char up)
+/******************************************************************************/
+/* Function:	setupbutton_event					      */
+/*		- Handle state changes of Setup button			      */
+/* History :	16 Feb 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+}
+/* setupbutton_event */
+
+
+/******************************************************************************/
+/* Local Implementations						      */
+/******************************************************************************/
+
+static void interrupt_init (void)
+{
+	PORTB_old = PORTB;
+
+	/* Enable peripheral interrupts */
+	PEIE = 1;
+
+	/* Enable interrupts */
+	GIE = 1;
+}
+
+static void interrupt isr (void)
+{
+	unsigned char temp;
+
+	/* Timer 1 interrupt */
+	if (TMR1IF) {
+		/* Reset interrupt */
+		TMR1IF = 0;
+		/* Handle interrupt */
+		timer_isr();
+	}
+	/* Timer 2 interrupt */
+	if (TMR2IF) {
+		/* Reset interrupt */
+		TMR2IF = 0;
+		/* Handle interrupt */
+		catsensor_isr_timer();
+	}
+	/* Port B interrupt */
+	if (RBIF) {
+		/* Reset interrupt */
+		RBIF = 0;
+		/* Detected changes */
+		temp = PORTB ^ PORTB_old;
+		/* Handle interrupt */
+		if (temp & 0x10)
+			catsensor_isr_input();
+		/* Update the old status */
+		PORTB_old = PORTB ;
+	}
+}
