d57efa9bcfa5110717870c8b6ace846843d3e6f1
CatGenius <robert@delien.nl>
2010-02-21 15:50:32 +0000

    Button behavior implemented

--- software/testprograms/catgenius/userinterface.c
+++ software/testprograms/catgenius/userinterface.c
@@ -24,7 +24,7 @@
 /* Global Data								      */
 /******************************************************************************/
 
-static struct timer	holdtimeout;
+static struct timer	holdtimeout	= {0xFFFF, 0xFFFFFFFF};
 static bit		muteupevent	= 0;
 static bit		locked		= 0;
 static bit		start_button	= 0;
@@ -33,6 +33,11 @@ static bit		setup_button	= 0;
 /******************************************************************************/
 /* Local Prototypes							      */
 /******************************************************************************/
+
+static void		setup	(void);
+static void		start	(void);
+static void		level	(void);
+static void		lock	(void);
 
 
 /******************************************************************************/
@@ -86,15 +91,34 @@ void startbutton_event (unsigned char up)
 	if (!up) {
 		start_button = 1;
 		settimeout(&holdtimeout, HOLDTIME);
-		if (!locked) {
-		} else {
+		if (setup_button)
+			muteupevent = 1;
+		if (!locked)
 			set_Beeper(0x01, 0);
-			if (setup_button)
-				muteupevent = 1;
-		}
+		else
+			set_LED_Locked(0x55, 1);
 	} else {
 		start_button = 0;
-		if (!setup_button);
+		if (!muteupevent) 
+			if (locked)
+				set_LED_Locked(0xFF, 1);
+			else
+				/* Handle setup up */
+				start();
+		else
+			if (!setup_button) {
+				if (timeoutexpired(&holdtimeout))
+					/* Handle long double-press up */
+					lock();
+				else
+					/* Handle short double-press up */
+					if (locked)
+						set_LED_Locked(0xFF, 1);
+					else
+						/* Handle level check */
+						level();
+				muteupevent = 0;
+			}
 	}
 }
 /* startbutton_event */
@@ -111,10 +135,66 @@ void setupbutton_event (unsigned char up)
 	if (!up) {
 		setup_button = 1;
 		settimeout(&holdtimeout, HOLDTIME);
+		if (start_button)
+			muteupevent = 1;
+		if (!locked)
+			set_Beeper(0x01, 0);
+		else
+			set_LED_Locked(0x55, 1);
 	} else {
 		setup_button = 0;
+		if (!muteupevent)
+			if (locked)
+				set_LED_Locked(0xFF, 1);
+			else
+				/* Handle setup up */
+				setup();
+		else
+			if (!start_button) {
+				if (timeoutexpired(&holdtimeout))
+					/* Handle long double-press up */
+					lock();
+				else
+					/* Handle short double-press up */
+					if (locked)
+						set_LED_Locked(0xFF, 1);
+					else
+						/* Handle level check */
+						level();
+				muteupevent = 0;
+			}
 	}
 }
 /* setupbutton_event */
 
 
+/******************************************************************************/
+/* Local Implementations						      */
+/******************************************************************************/
+
+static void setup (void)
+{
+	set_Beeper(0x05, 0);
+}
+
+
+static void start (void)
+{
+	set_Beeper(0x05, 0);
+}
+
+
+static void level (void)
+{
+	set_Beeper(0x15, 0);
+}
+
+
+static void lock (void)
+{
+	locked = !locked;
+	if (locked)
+		set_LED_Locked(0xFF, 1);
+	else
+		set_LED_Locked(0x00, 0);
+}
