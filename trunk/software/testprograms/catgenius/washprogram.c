5766359c2582aaf22e04b1c85108aac5f0d82043
robert <robert@fd22e926-ac0d-87e4-e151-82697668f4d6>
2010-02-25 19:50:09 +0000

    Implemented the remaining part of the standard washing program

--- trunk/software/testprograms/catgenius/washprogram.c
+++ trunk/software/testprograms/catgenius/washprogram.c
@@ -141,7 +141,7 @@ static const struct command	program[] = {
 	{CMD_PUMP,	0},		/* Wash + 9 */
 	{CMD_WAITTIME,	8202},
 	{CMD_PUMP,	1},		/* Wash + 10 */
-	{CMD_WAITTIME,	65616},		/* Delay split in two because it exceeds maximum */
+	{CMD_WAITTIME,	65535},		/* Delay split in two because it exceeds maximum */
 	{CMD_WAITTIME,	81},		/* 65535 + 81 = 65616 */
 	{CMD_PUMP,	0},		/* Wash + 11 */
 	{CMD_WATER,	1},
@@ -157,7 +157,8 @@ static const struct command	program[] = {
 	{CMD_PUMP,	1},		/* Wash + 15 */
 	{CMD_WAITTIME,	25206},
 	{CMD_PUMP,	0},		/* Wash + 16 */
-	{CMD_WAITTIME,	75718},	/* TODO: Split up */
+	{CMD_WAITTIME,	65535},		/* Delay split in two because it exceeds maximum */
+	{CMD_WAITTIME,	10183},		/* 65535 + 10183 = 75718 */
 	{CMD_PUMP,	1},		/* Wash + 17 */
 	{CMD_WAITTIME,	24206},
 	{CMD_PUMP,	0},		/* Wash + 18 */
@@ -167,7 +168,8 @@ static const struct command	program[] = {
 	{CMD_PUMP,	0},		/* Wash + 20 */
 	{CMD_WAITTIME,	8202},
 	{CMD_PUMP,	1},		/* Wash + 21 */
-	{CMD_WAITTIME,	75718},	/* TODO: Split up */
+	{CMD_WAITTIME,	65535},		/* Delay split in two because it exceeds maximum */
+	{CMD_WAITTIME,	10183},		/* 65535 + 10183 = 75718 */
 	{CMD_PUMP,	0},		/* Wash + 22 */
 	{CMD_WATER,	1},
 	{CMD_WAITTIME,	25502},
@@ -189,16 +191,127 @@ static const struct command	program[] = {
 	{CMD_WAITTIME,	39107},
 	{CMD_PUMP,	1},		/* Wash + 29 */
 	{CMD_BOWL,	BOWL_CCW},
-	{CMD_WAITTIME,	65648},	/* TODO: Split up */
+	{CMD_WAITTIME,	65535},		/* Delay split in two because it exceeds maximum */
+	{CMD_WAITTIME,	113},		/* 65535 + 113 = 65648 */
 	{CMD_BOWL,	BOWL_CW},	/* Wash + 30 */
 	{CMD_WAITTIME,	10075},
 	{CMD_BOWL,	BOWL_STOP},	/* Wash + 31 */
 	{CMD_DOSAGE,	1},
 	{CMD_WAITTIME,	9802},
 	{CMD_BOWL,	BOWL_CW},	/* Wash + 32 */
-	{CMD_DOSAGE,	1},
+	{CMD_DOSAGE,	0},
 	{CMD_WAITTIME,	25270},
-
+	{CMD_BOWL,	BOWL_CCW},	/* Dry */
+	{CMD_DRYER,	1},
+	{CMD_WAITTIME,	35372},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 1 */
+	{CMD_WAITTIME,	55513},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 2 */
+	{CMD_WAITTIME,	35277},
+	{CMD_PUMP,	0},		/* Dry + 3 */
+	{CMD_WAITTIME,	1732},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 4 */
+	{CMD_WAITTIME,	55514},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 5 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 6 */
+	{CMD_WAITTIME,	55514},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 7 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 8 */
+	{CMD_WAITTIME,	45411},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 9 */
+	{CMD_WAITTIME,	35404},
+	{CMD_ARM,	ARM_UP},	/* Dry + 10 */
+	{CMD_WAITTIME,	9980},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 11 */
+	{CMD_BOWL,	BOWL_STOP},
+	{CMD_DOSAGE,	1},
+	{CMD_WAITTIME,	2601},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 12 */
+	{CMD_DOSAGE,	0},
+	{CMD_WAITTIME,	5392},
+	{CMD_ARM,	ARM_DOWN},	/* Dry + 13 */
+	{CMD_WAITTIME,	10303},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 14 */
+	{CMD_WAITTIME,	35245},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 15 */
+	{CMD_WAITTIME,	45411},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 16 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 17 */
+	{CMD_WAITTIME,	45411},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 18 */
+	{CMD_WAITTIME,	35404},
+	{CMD_ARM,	ARM_UP},	/* Dry + 19 */
+	{CMD_WAITTIME,	10380},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 20 */
+	{CMD_BOWL,	BOWL_STOP},
+	{CMD_DOSAGE,	1},
+	{CMD_WAITTIME,	2601},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 21 */
+	{CMD_DOSAGE,	0},
+	{CMD_WAITTIME,	4392},
+	{CMD_ARM,	ARM_DOWN},	/* Dry + 22 */
+	{CMD_WAITTIME,	10803},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 23 */
+	{CMD_WAITTIME,	35245},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 24 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 25 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 26 */
+	{CMD_WAITTIME,	35308},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 27 */
+	{CMD_WAITTIME,	35404},
+	{CMD_ARM,	ARM_UP},	/* Dry + 28 */
+	{CMD_WAITTIME,	11503},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 29 */
+	{CMD_WAITTIME,	2169},
+	{CMD_ARM,	ARM_DOWN},	/* Dry + 30 */
+	{CMD_WAITTIME,	11703},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 31 */
+	{CMD_WAITTIME,	45347},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 32 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 33 */
+	{CMD_WAITTIME,	45411},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 34 */
+	{CMD_WAITTIME,	35309},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 35 */
+	{CMD_WAITTIME,	45411},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 36 */
+	{CMD_WAITTIME,	35308},
+	{CMD_BOWL,	BOWL_CCW},	/* Dry + 37 */
+	{CMD_WAITTIME,	45411},
+	{CMD_BOWL,	BOWL_CW},	/* Dry + 38 */
+	{CMD_WAITTIME,	35404},
+	{CMD_ARM,	ARM_UP},	/* Dry + 39 */
+	{CMD_WAITTIME,	300},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 40 */
+	{CMD_WAITTIME,	12203},
+	{CMD_ARM,	ARM_UP},	/* Dry + 41 */
+	{CMD_WAITTIME,	300},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 42 */
+	{CMD_WAITTIME,	12203},
+	{CMD_ARM,	ARM_UP},	/* Dry + 43 */
+	{CMD_WAITTIME,	300},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 44 */
+	{CMD_WAITTIME,	10203},
+	{CMD_ARM,	ARM_UP},	/* Dry + 45 */
+	{CMD_WAITTIME,	300},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 46 */
+	{CMD_WAITTIME,	10202},
+	{CMD_ARM,	ARM_UP},	/* Dry + 47 */
+	{CMD_WAITTIME,	9170},
+	{CMD_ARM,	ARM_DOWN},	/* Dry + 48 */
+	{CMD_WAITTIME,	6474},
+	{CMD_ARM,	ARM_UP},	/* Dry + 49 */
+	{CMD_WAITTIME,	18268},
+	{CMD_ARM,	ARM_DOWN},	/* Dry + 50 */
+	{CMD_BOWL,	BOWL_STOP},
+	{CMD_WAITTIME,	2832},
+	{CMD_ARM,	ARM_STOP},	/* Dry + 51 */
 	{CMD_END,	0}
 };
 
