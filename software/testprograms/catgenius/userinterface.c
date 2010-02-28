262642de49ed297a64b861480b47b934ca8d1ae3
CatGenius <robert@delien.nl>
2010-02-28 12:34:58 +0000

    Cartridge level added to user interface

--- software/testprograms/catgenius/userinterface.c
+++ software/testprograms/catgenius/userinterface.c
@@ -19,25 +19,47 @@
 /******************************************************************************/
 
 #define HOLDTIME		(3 * SECOND)
+#define LEVEL_TIMEOUT		(2 * SECOND)
 
+#define DISP_AUTOMODE		0
+#define DISP_CARTRIDGELEVEL	1
+#define DISP_ERROR		2
+
+#define AUTO_MANUAL		0
+#define AUTO_TIMED1		1
+#define AUTO_TIMED2		2
+#define AUTO_TIMED3		3
+#define AUTO_TIMED4		4
+#define AUTO_DETECTED1ON1	5
+#define AUTO_DETECTED1ON2	6
+#define AUTO_DETECTED1ON3	7
+#define AUTO_DETECTED1ON4	8
+#define AUTO_DETECTED		9
 
 /******************************************************************************/
 /* Global Data								      */
 /******************************************************************************/
 
-static struct timer	holdtimeout	= {0xFFFF, 0xFFFFFFFF};
+static struct timer	cartridgetimeout= EXIRED;
+static struct timer	holdtimeout	= NEVER;
+
+/* Keyboard status bits */
 static bit		muteupevent	= 0;
 static bit		locked		= 0;
 static bit		start_button	= 0;
 static bit		setup_button	= 0;
 
-static unsigned char	mode		= 0;
+static unsigned char	disp_mode	= DISP_AUTOMODE;
+static unsigned char	auto_mode	= AUTO_MANUAL;
+static unsigned char	cart_level	= 100;
+static unsigned char	error_nr	= 0;
 
 
 /******************************************************************************/
 /* Local Prototypes							      */
 /******************************************************************************/
 
+static void		update_display	(unsigned char mode);
 static void		setup	(void);
 static void		start	(void);
 static void		level	(void);
@@ -68,6 +90,14 @@ void userinterface_work (void)
 /*		- Initial revision.					      */
 /******************************************************************************/
 {
+	if( (disp_mode == DISP_CARTRIDGELEVEL) &&
+	    (timeoutexpired(&cartridgetimeout)) ) {
+		if (error_nr)
+			disp_mode = DISP_ERROR;
+		else
+			disp_mode = DISP_AUTOMODE;
+		update_display(disp_mode);
+	}
 }
 /* userinterface_work */
 
@@ -176,86 +206,156 @@ void setupbutton_event (unsigned char up)
 /* Local Implementations						      */
 /******************************************************************************/
 
-static void setup (void)
+static void update_display (unsigned char mode)
 {
-	if (++mode > 9)
-		mode = 0;
-	switch (mode) {
+	if (mode != disp_mode)
+		return;
+	switch (disp_mode) {
 	default:
-		mode = 0;
-	case 0:
-		set_LED(1, 0);
-		set_LED(2, 0);
-		set_LED(3, 0);
-		set_LED(4, 0);
-		set_LED_Cat(0x00, 0);
+		disp_mode = DISP_AUTOMODE;
+	case DISP_AUTOMODE:
+		set_LED_Error(0x00, 0);
+		if (cart_level >= 10)
+			set_LED_Cartridge(0x00, 0);
+		else
+			set_LED_Cartridge(0x55, 1);
+		switch (auto_mode) {
+		default:
+			auto_mode = 0;
+		case AUTO_MANUAL:
+			set_LED(1, 0);
+			set_LED(2, 0);
+			set_LED(3, 0);
+			set_LED(4, 0);
+			set_LED_Cat(0x00, 0);
+			break;
+		case AUTO_TIMED1:
+			set_LED(1, 1);
+			set_LED(2, 0);
+			set_LED(3, 0);
+			set_LED(4, 0);
+			set_LED_Cat(0x00, 0);
+			break;
+		case AUTO_TIMED2:
+			set_LED(1, 0);
+			set_LED(2, 1);
+			set_LED(3, 0);
+			set_LED(4, 0);
+			set_LED_Cat(0x00, 0);
+			break;
+		case AUTO_TIMED3:
+			set_LED(1, 0);
+			set_LED(2, 0);
+			set_LED(3, 1);
+			set_LED(4, 0);
+			set_LED_Cat(0x00, 0);
+			break;
+		case AUTO_TIMED4:
+			set_LED(1, 0);
+			set_LED(2, 0);
+			set_LED(3, 0);
+			set_LED(4, 1);
+			set_LED_Cat(0x00, 0);
+			break;
+		case AUTO_DETECTED1ON1:
+			set_LED(1, 1);
+			set_LED(2, 0);
+			set_LED(3, 0);
+			set_LED(4, 0);
+			set_LED_Cat(0xFF, 1);
+			break;
+		case AUTO_DETECTED1ON2:
+			set_LED(1, 1);
+			set_LED(2, 1);
+			set_LED(3, 0);
+			set_LED(4, 0);
+			set_LED_Cat(0xFF, 1);
+			break;
+		case AUTO_DETECTED1ON3:
+			set_LED(1, 1);
+			set_LED(2, 0);
+			set_LED(3, 1);
+			set_LED(4, 0);
+			set_LED_Cat(0xFF, 1);
+			break;
+		case AUTO_DETECTED1ON4:
+			set_LED(1, 1);
+			set_LED(2, 0);
+			set_LED(3, 0);
+			set_LED(4, 1);
+			set_LED_Cat(0xFF, 1);
+			break;
+		case AUTO_DETECTED:
+			set_LED(1, 0);
+			set_LED(2, 0);
+			set_LED(3, 0);
+			set_LED(4, 0);
+			set_LED_Cat(0xFF, 1);
+			break;
+		}
 		break;
-	case 1:
-		set_LED(1, 1);
-		set_LED(2, 0);
-		set_LED(3, 0);
-		set_LED(4, 0);
-		set_LED_Cat(0x00, 0);
+	case DISP_CARTRIDGELEVEL:
+		set_LED_Error(0x00, 0);
+		set_LED_Cartridge(0xFF, 1);
+		set_LED(1, cart_level >= 10);
+		set_LED(2, cart_level >= 25);
+		set_LED(3, cart_level >= 50);
+		set_LED(4, cart_level >= 75);
+		if (cart_level >= 100)
+			set_LED_Cat(0xFF, 1);
+		else
+			set_LED_Cat(0x00, 0);
 		break;
-	case 2:
-		set_LED(1, 0);
-		set_LED(2, 1);
-		set_LED(3, 0);
-		set_LED(4, 0);
-		set_LED_Cat(0x00, 0);
-		break;
-	case 3:
-		set_LED(1, 0);
-		set_LED(2, 0);
-		set_LED(3, 1);
-		set_LED(4, 0);
-		set_LED_Cat(0x00, 0);
-		break;
-	case 4:
-		set_LED(1, 0);
-		set_LED(2, 0);
-		set_LED(3, 0);
-		set_LED(4, 1);
-		set_LED_Cat(0x00, 0);
-		break;
-	case 5:
-		set_LED(1, 1);
-		set_LED(2, 0);
-		set_LED(3, 0);
-		set_LED(4, 0);
-		set_LED_Cat(0xFF, 1);
-		break;
-	case 6:
-		set_LED(1, 1);
-		set_LED(2, 1);
-		set_LED(3, 0);
-		set_LED(4, 0);
-		set_LED_Cat(0xFF, 1);
-		break;
-	case 7:
-		set_LED(1, 1);
-		set_LED(2, 0);
-		set_LED(3, 1);
-		set_LED(4, 0);
-		set_LED_Cat(0xFF, 1);
-		break;
-	case 8:
-		set_LED(1, 1);
-		set_LED(2, 0);
-		set_LED(3, 0);
-		set_LED(4, 1);
-		set_LED_Cat(0xFF, 1);
-		break;
-	case 9:
-		set_LED(1, 0);
-		set_LED(2, 0);
-		set_LED(3, 0);
-		set_LED(4, 0);
-		set_LED_Cat(0xFF, 1);
+	case DISP_ERROR:
+		set_LED_Error(0x55, 1);
+		set_LED_Cartridge(0x00, 0);
+		set_LED(1, error_nr == 1);
+		set_LED(2, error_nr == 2);
+		set_LED(3, error_nr == 3);
+		set_LED(4, error_nr == 4);
 		break;
 	}
 }
 
+
+static void setup (void)
+{
+	switch (disp_mode) {
+	default:
+		disp_mode = DISP_AUTOMODE;
+	case DISP_AUTOMODE:
+		/* Increase auto mode */
+		if (++auto_mode > AUTO_DETECTED)
+			auto_mode = AUTO_MANUAL;
+		/* Reset timers and cat sensor */
+
+		/* Update the display */
+		update_display(disp_mode);
+		break;
+
+	case DISP_CARTRIDGELEVEL:
+		if (cart_level < 10)
+			cart_level = 10;
+		else if (cart_level < 25)
+			cart_level = 25;
+		else if (cart_level < 50)
+			cart_level = 50;
+		else if (cart_level < 75)
+			cart_level = 75;
+		else if (cart_level < 100)
+			cart_level = 100;
+		else
+			cart_level = 0;
+		/* Set new timeout to return to auto- or errormode */
+		settimeout(&cartridgetimeout, LEVEL_TIMEOUT);
+		/* Update the display */
+		update_display(disp_mode);
+		break;
+
+	case DISP_ERROR:
+		break;
+	}
+}
 
 static void start (void)
 {
@@ -266,7 +366,12 @@ static void start (void)
 
 static void level (void)
 {
-	set_Beeper(0x15, 0);
+	/* Swich display to cartridge level mode */
+	disp_mode = DISP_CARTRIDGELEVEL;
+	/* Set timeout to return to auto- or errormode */
+	settimeout(&cartridgetimeout, LEVEL_TIMEOUT);
+	/* Update the display */
+	update_display(disp_mode);
 }
 
 
