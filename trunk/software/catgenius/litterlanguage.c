b254d3ea631f308a3f88681bd8cbfff7084d5c59
robert <robert@fd22e926-ac0d-87e4-e151-82697668f4d6>
2010-05-31 19:59:02 +0000

    Reject programs not beginning with a LitterLanguage CMD_START

--- trunk/software/catgenius/litterlanguage.c
+++ trunk/software/catgenius/litterlanguage.c
@@ -24,6 +24,12 @@
 #define BOX_MESSY	1
 #define BOX_WET		2
 
+#define STATE_IDLE		0
+#define STATE_FETCH_START	1
+#define STATE_GET_START		2
+#define STATE_FETCH_CMD		3
+#define STATE_GET_CMD		4
+#define STATE_WAIT_CMD		5
 
 /******************************************************************************/
 /* Global Data								      */
@@ -33,7 +39,7 @@ static unsigned char	prg_source	= 0;
 static bit		wet_program	= 0;
 static bit		water_detected	= 0;
 
-static unsigned char	cmd_state	= 0;
+static unsigned char	cmd_state	= STATE_IDLE;
 static struct command	const * cmd_pointer	= 0;
 static struct command	cur_command;
 
@@ -136,20 +142,46 @@ void litterlanguage_work (void)
 	}
 
 	switch (cmd_state) {
-	case 0:	/* Idle */
+	case STATE_IDLE:		/* Idle */
 		break;
-	case 1:	/* Fetch a new command*/
+	case STATE_FETCH_START:	/* Fetch the start command*/
 		req_command(cmd_pointer);
-		cmd_state ++;
+		cmd_state = STATE_GET_START;
+		/* no break; */
+	case STATE_GET_START:	/* Wait for the start command to be fetched */
+		if (get_command(&cur_command)) {
+			DBG("IP 0x%04X: ", cmd_pointer);
+			if (cur_command.cmd == CMD_START) {
+				DBG("CMD_START, %s", wet_program?"wet":"dry");
+				/* Check if this is a valid program for us */
+				if( ((cur_command.arg & 0x00FF) <= CMD_END) && 
+				    ( (!wet_program && (cur_command.arg & FLAGS_DRYRUN)) ||
+				      (wet_program && (cur_command.arg & FLAGS_WETRUN)) ) ) {
+					eeprom_write(NVM_BOXSTATE, BOX_MESSY);
+					cmd_pointer++;
+					cmd_state = STATE_FETCH_CMD;
+				} else {
+					cmd_state = STATE_IDLE;
+					DBG(", incompatible");
+				}
+			} else {
+				cmd_state = STATE_IDLE;
+				DBG(", no start: 0x%X", cur_command.cmd);
+			}
+			DBG("\n");
+		}
 		break;
-	case 2:	/* Wait for the new command to be fetched */
-		if (get_command(&cur_command))
-			cmd_state ++;
+	case STATE_FETCH_CMD:	/* Fetch the next command*/
+		req_command(cmd_pointer);
+		cmd_state = STATE_GET_CMD;
+		/* no break; */
+	case STATE_GET_CMD:	/* Wait for the start command to be fetched */
+		if (get_command(&cur_command)) {
+			/* Decode and execute the command */
+			exe_command();
+		}
 		break;
-	case 3:	/* Decode and execute the command */
-		exe_command();
-		break;
-	case 4:	/* Wait for the command to finish */
+	case STATE_WAIT_CMD:	/* Wait for the command to finish */
 		wait_command();
 		break;
 	}
@@ -172,21 +204,21 @@ void litterlanguage_term (void)
 void litterlanguage_start (unsigned char wet)
 {
 	extern const struct command	washprogram[];
-	if (!cmd_state) {
+	if (cmd_state == STATE_IDLE) {
 		switch (prg_source) {
 		case SRC_ROM:
 			cmd_pointer = &washprogram[0];
 			break;
 		}
 		wet_program = wet;
-		cmd_state ++ ;
+		cmd_state = STATE_FETCH_START ;
 	}
 }
 
 
 unsigned char litterlanguage_running (void)
 {
-	return (cmd_state);
+	return (cmd_state != STATE_IDLE);
 }
 
 
@@ -197,7 +229,7 @@ void litterlanguage_pause (unsigned char pause)
 
 void litterlanguage_stop (void)
 {
-	if (cmd_state) {
+	if (cmd_state != STATE_IDLE) {
 		/* Stop all actuators */
 		set_Bowl(BOWL_STOP);
 		set_Arm(ARM_STOP);
@@ -205,11 +237,12 @@ void litterlanguage_stop (void)
 		set_Dosage(0);
 		set_Pump(0);
 		set_Dryer(0);
-		/* Disable timeouts */
+		/* Disable all timeouts */
 		timeoutnever(&timer_fill);
 		timeoutnever(&timer_drain);
 		timeoutnever(&timer_autodose);
-		cmd_state = 0;
+		/* Stop the state machine */
+		cmd_state = STATE_IDLE;
 	}
 }
 
@@ -225,7 +258,7 @@ void watersensor_event (unsigned char undetected)
 	water_detected = !undetected;
 
 	DBG("Water %s\n", water_detected?"high":"low");
-	if (cmd_state) {
+	if (cmd_state != STATE_IDLE) {
 		/* Disable timeout on event */
 		if (water_detected) {
 			/* Turn off the water and disable the timeout */
@@ -246,11 +279,11 @@ void watersensor_event (unsigned char undetected)
 static void litterlanguage_cleanup (unsigned char wet)
 {
 	extern const struct command	cleanupprogram[];
-	if (!cmd_state) {
+	if (cmd_state == STATE_IDLE) {
 		prg_source = SRC_ROM;
 		cmd_pointer = &cleanupprogram[0];
 		wet_program = wet;
-		cmd_state ++ ;
+		cmd_state = STATE_FETCH_START ;
 	}
 }
 
@@ -278,31 +311,17 @@ static void exe_command (void)
 
 	DBG("IP 0x%04X: ", cmd_pointer);
 	switch (cur_command.cmd) {
-	case CMD_START:
-		DBG("CMD_START, %s", wet_program?"wet":"dry");
-		/* Check if this is a valid program for us */
-		if( ((cur_command.arg & 0x00FF) <= CMD_END) && 
-		    ( (!wet_program && (cur_command.arg & FLAGS_DRYRUN)) ||
-		      (wet_program && (cur_command.arg & FLAGS_WETRUN)) ) ) {
-			eeprom_write(NVM_BOXSTATE, BOX_MESSY);
-			cmd_pointer++;
-			cmd_state -= 2;
-		} else {
-			cmd_state = 0;
-			DBG(", failed");
-		}
-		break;
 	case CMD_BOWL:
 		DBG("CMD_BOWL, %s", (cur_command.arg == BOWL_STOP)?"BOWL_STOP":((cur_command.arg == BOWL_CW)?"BOWL_CW":"BOWL_CCW"));
 		set_Bowl((unsigned char)cur_command.arg);
 		cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_ARM:
 		DBG("CMD_ARM, %s", (cur_command.arg == ARM_STOP)?"ARM_STOP":((cur_command.arg == ARM_DOWN)?"ARM_DOWN":"ARM_UP"));
 		set_Arm((unsigned char)cur_command.arg);
 		cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_WATER:
 		DBG("CMD_WATER, %s%s", cur_command.arg?"on":"off", wet_program?"":" (nop)");
@@ -321,27 +340,27 @@ static void exe_command (void)
 				timeoutnever(&timer_fill);
 			}
 		cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_PUMP:
 		DBG("CMD_PUMP, %s%s", cur_command.arg?"on":"off", wet_program?"":" (nop)");
 		if (wet_program)
 			set_Pump((unsigned char)cur_command.arg);
 		cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_DRYER:
 		DBG("CMD_DRYER, %s%s", cur_command.arg?"on":"off", wet_program?"":" (nop)");
 		if (wet_program)
 			set_Dryer((unsigned char)cur_command.arg);
 		cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_WAITTIME:
 		DBG("CMD_WAITTIME, %ums", cur_command.arg);
 		settimeout( &timer_waitcmd,
 			    (unsigned long)cur_command.arg * MILISECOND );
-		cmd_state++;
+		cmd_state = STATE_WAIT_CMD;
 		break;
 	case CMD_WAITWATER:
 		DBG("CMD_WAITWATER, %s%s", cur_command.arg?"high":"low", wet_program?"":" (nop)");
@@ -349,19 +368,19 @@ static void exe_command (void)
 			if (!cur_command.arg)
 				/* Start the drain timeout, we don't want to wait forever */
 				settimeout(&timer_drain, MAX_DRAINTIME);
-			cmd_state++;
+			cmd_state = STATE_WAIT_CMD;
 		} else {
 			cmd_pointer++;
-			cmd_state -= 2;
+			cmd_state = STATE_FETCH_CMD;
 		}
 		break;
 	case CMD_WAITDOSAGE:
 		DBG("CMD_WAITDOSAGE%s", wet_program?"":" (nop)");
 		if (wet_program)
-			cmd_state++;
+			cmd_state = STATE_WAIT_CMD;
 		else {
 			cmd_pointer++;
-			cmd_state -= 2;
+			cmd_state = STATE_FETCH_CMD;
 		}
 		break;
 	case CMD_SKIPIFDRY:
@@ -370,7 +389,7 @@ static void exe_command (void)
 			cmd_pointer += cur_command.arg + 1;
 		else
 			cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_SKIPIFWET:
 		DBG("CMD_SKIPIFWET, %u%s", cur_command.arg, wet_program?"":" (nop)");
@@ -378,7 +397,7 @@ static void exe_command (void)
 			cmd_pointer += cur_command.arg + 1;
 		else
 			cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_AUTODOSE:
 		DBG("CMD_AUTODOSE, %u.%uml%s", cur_command.arg/10, cur_command.arg%10, wet_program?"":" (nop)");
@@ -388,22 +407,26 @@ static void exe_command (void)
 			set_Dosage(1);
 		}
 		cmd_pointer++;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_CALL:
 		DBG("CMD_CALL, 0x%04X", cur_command.arg);
 		ret_address = cmd_pointer + 1;
 		cmd_pointer = (struct command const*)cur_command.arg;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_RETURN:
 		DBG("CMD_RETURN, 0x%04X", ret_address);
 		cmd_pointer = ret_address;
-		cmd_state -= 2;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	case CMD_END:
 		DBG("CMD_END");
 		eeprom_write(NVM_BOXSTATE, BOX_TIDY);
+		litterlanguage_stop();
+		break;
+	case CMD_START:
+		DBG("CMD_START, unexpected");
 		litterlanguage_stop();
 		break;
 	default:
@@ -421,33 +444,33 @@ static void wait_command (void)
 	case CMD_WAITTIME:
 		if (timeoutexpired(&timer_waitcmd)) {
 			cmd_pointer++;
-			cmd_state -= 3;
+			cmd_state = STATE_FETCH_CMD;
 		}
 		break;
 	case CMD_WAITWATER:
 		if (cur_command.arg) {
 			if (water_detected) {
 				cmd_pointer++;
-				cmd_state -= 3;
+				cmd_state = STATE_FETCH_CMD;
 			}
 		} else {
 			if (!water_detected) {
 				/* Disable the timeout */
 				timeoutnever(&timer_drain);
 				cmd_pointer++;
-				cmd_state -= 3;
+				cmd_state = STATE_FETCH_CMD;
 			}
 		}
 		break;
 	case CMD_WAITDOSAGE:
 		if (!get_Dosage()) {
 			cmd_pointer++;
-			cmd_state -= 3;
+			cmd_state = STATE_FETCH_CMD;
 		}
 		break;
 	default:
 		cmd_pointer++;
-		cmd_state -= 3;
+		cmd_state = STATE_FETCH_CMD;
 		break;
 	}
 }
