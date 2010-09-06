e0f476400427f13bfc1f9cb245fc196810b82787
robert <robert@fd22e926-ac0d-87e4-e151-82697668f4d6>
2010-09-06 08:33:32 +0000

    Missing rtc.c added, erroneous rtc.h.bak removed

---
+++ trunk/software/common/rtc.c
@@ -0,0 +1,136 @@
+/******************************************************************************/
+/* File    :	rtc.c							      */
+/* Function:	Real Time Clock						      */
+/* Author  :	Robert Delien						      */
+/*		Copyright (C) 2010, Clockwork Engineering		      */
+/* History :	3 Sep 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+#include <htc.h>
+#include <stdio.h>
+
+#include "rtc.h"
+#include "timer.h"
+#include "hardware.h"
+#include "catgenie120.h"
+
+
+/******************************************************************************/
+/* Macros								      */
+/******************************************************************************/
+
+
+/******************************************************************************/
+/* Global Data								      */
+/******************************************************************************/
+
+static struct timer	second;
+static unsigned long	uptime = 0;
+static struct time	currenttime;
+
+
+/******************************************************************************/
+/* Local Prototypes							      */
+/******************************************************************************/
+
+
+/******************************************************************************/
+/* Global Implementations						      */
+/******************************************************************************/
+
+void rtc_init (unsigned char flags)
+/******************************************************************************/
+/* Function:	rtc_init						      */
+/*		- Initialize the real time clock			      */
+/* History :	3 Sep 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+	settimeout(&second, SECOND);
+
+	if (flags & POWER_FAILURE) {
+		currenttime.seconds = 0;
+		currenttime.minutes = 0;
+		currenttime.hours   = 0;  
+		currenttime.weekday = 0;  
+	}
+}
+/* rtc_init */
+
+
+void rtc_work (void)
+/******************************************************************************/
+/* Function:	rtc_work						      */
+/*		- Worker function for the real time clock		      */
+/* History :	3 Sep 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+	if (timeoutexpired(&second)) {
+		postponetimeout(&second, SECOND);
+		uptime++;
+		if (++currenttime.seconds >= 60) {
+			currenttime.seconds = 0;
+			if (++currenttime.minutes >=60) {
+				currenttime.minutes = 0;
+				if (++currenttime.hours >=24) {
+					currenttime.hours = 0;
+					if (++currenttime.weekday >=7)
+						currenttime.weekday = 0;
+				}
+			}
+		}
+	}
+}
+/* rtc_work */
+
+
+void rtc_term (void)
+/******************************************************************************/
+/* Function:	rtc_term						      */
+/*		- Terminate the real time clock				      */
+/* History :	3 Sep 2010 by R. Delien:				      */
+/*		- Initial revision.					      */
+/******************************************************************************/
+{
+}
+/* rtc_term */
+
+
+void printtime (void)
+{
+	printf( "%d %d:%.2d.%.2d\n",
+		currenttime.weekday, currenttime.hours,
+		currenttime.minutes, currenttime.seconds );
+}
+
+
+void incminutes (void)
+{
+	postponetimeout(&second, SECOND);
+	if (++currenttime.minutes >=60)
+		currenttime.minutes = 0;
+	printtime();
+}
+
+
+void inchours (void)
+{
+	postponetimeout(&second, SECOND);
+	if (++currenttime.hours >=23)
+		currenttime.hours = 0;
+}
+
+
+void incweekday (void)
+{
+	postponetimeout(&second, SECOND);
+	if (++currenttime.weekday >=7)
+		currenttime.weekday = 0;
+}
+
+
+/******************************************************************************/
+/* Local Implementations						      */
+/******************************************************************************/
+
