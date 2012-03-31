2fd3af6591407bc4d94b2f30ea1678b7fd8af8a0
robert.delien <robert.delien@fd22e926-ac0d-87e4-e151-82697668f4d6>
2012-03-31 20:22:43 +0000

    - Added command line command 'tag' to dump the cartridge chip contents

---
+++ trunk/software/common/cmdline_tag.h
@@ -0,0 +1,14 @@
+/******************************************************************************/
+/* File    :	cmdline_tag.h						      */
+/* Function:	Include file of 'cmdline_tag.c'.			      */
+/* Author  :	Robert Delien						      */
+/*		Copyright (C) 2012, Clockwork Engineering		      */
+/******************************************************************************/
+
+#ifndef CMDLINE_TAG_H			/* Include file already compiled? */
+#define CMDLINE_TAG_H
+
+/* Command implementations */
+int		tag			(char	*args) ;
+
+#endif /* CMDLINE_TAG_H */
