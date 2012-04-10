f9e6e56f7ba060888ddd97d0e75796b090d7344f
robert.delien <robert.delien@fd22e926-ac0d-87e4-e151-82697668f4d6>
2012-04-10 20:25:28 +0000

    - CatGenie model 120plus support added in CatGenius - Dead tag reader code removed form CatGenius - CatGenie model 120/120plus selection by CPU type selection

--- trunk/software/common/hardware.h
+++ trunk/software/common/hardware.h
@@ -13,7 +13,8 @@
 #else
    /*
     * If you get this error, you need to add either one of the above
-    * (HW_CATGENIE120, HW_CARTRIDGEGENIUS_WS0 or HW_CARTRIDGEGENIUS_WS1)
+    * (HW_CATGENIE120, HW_CATGENIE120PLUS,
+    *  HW_CARTRIDGEGENIUS_WS0 or HW_CARTRIDGEGENIUS_WS1)
     * to your project settings:
     * Project -> Build Options... -> Project
     * Tab 'Compiler', Define marcros, Add
