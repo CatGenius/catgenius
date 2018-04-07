4818451649eb8f26e7b67f067ca6324008ce1d90
Robert Delien <robert@delien.nl>
2018-04-07 12:34:03 +0200

    [io tester] Added dependencies in io_tester_prefs.h

--- software/iotester/iotester_prefs.h
+++ software/iotester/iotester_prefs.h
@@ -11,8 +11,17 @@
  * Select the options you wish to enable here
  * Approximate program memory requirements are listed to the right
  */
-#define HAS_SERIAL
-#define HAS_COMMANDLINE
 #define HAS_COMMANDLINE_GPIO
+
+/*
+ * Automatically include dependencies (DO NOT CHANGE)
+ */
+#if (defined HAS_COMMANDLINE_GPIO)
+#  define HAS_COMMANDLINE
+#endif
+
+#if (defined HAS_COMMANDLINE)
+#  define HAS_SERIAL
+#endif
 
 #endif /* IOTESTER_PREFS_H */
